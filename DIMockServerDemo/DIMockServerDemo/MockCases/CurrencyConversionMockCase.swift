6acdbc3b626a24968e143d57361b774f3db2a7cf
Przemysław Szurmak <edpeix@gmail.com>
2017-12-13 14:19:40 +0100

    add custom prices for PLN and AUD currencies in mock case

--- DIMockServerDemo/DIMockServerDemo/MockCases/CurrencyConversionMockCase.swift
+++ DIMockServerDemo/DIMockServerDemo/MockCases/CurrencyConversionMockCase.swift
@@ -15,20 +15,32 @@ class CurrencyConversionMockCase: DemoMockCase {
         
         
         let btcPriceUsd = String(1000)
-        let bitcoin = [CoinStructure.id: "btc",
+        let bitcoin: [String : Any] = [CoinStructure.id: "btc",
                        CoinStructure.name: "Bitcoin",
                        CoinStructure.symbol: "BTC",
                        CoinStructure.rank: "1",
                        CoinStructure.priceUsd: btcPriceUsd,
-                       CoinStructure.priceBtc: "1.0"]
+                       CoinStructure.priceBtc: "1.0",
+                       "ticker_prices": [
+                        [ "price_key": CoinStructure.currencyKey(currency: "PLN"),
+                          "price_key_value": "123.0"],
+                        [ "price_key": CoinStructure.currencyKey(currency: "AUD"),
+                          "price_key_value": "999.99"]]
+        ]
         
         let ethPriceUsd = String(10)
-        let ethereum = [CoinStructure.id: "ethereum",
+        let ethereum: [String : Any] = [CoinStructure.id: "ethereum",
                         CoinStructure.name: "Ethereum",
                         CoinStructure.symbol: "ETH",
                         CoinStructure.rank: "2",
                         CoinStructure.priceUsd: ethPriceUsd,
-                        CoinStructure.priceBtc: "0.02126"]
+                        CoinStructure.priceBtc: "0.02126",
+                        "ticker_prices": [
+                            [ "price_key": CoinStructure.currencyKey(currency: "PLN"),
+                              "price_key_value": "4343.0"],
+                            [ "price_key": CoinStructure.currencyKey(currency: "AUD"),
+                              "price_key_value": "777.0"]]
+        ]
         
         let tickers = TemplateBuilder.buildTemplate(name: "Tickers", with: ["tickers":[bitcoin, ethereum]])
         return jsonStringResponse(tickers)
